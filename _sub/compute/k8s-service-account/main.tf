b98266d19a6ef715f0663af238d3c9521cd8edc1
Emil H. Clausen <sequoiia@hummel.yt>
2020-10-08 11:32:07 +0200

    Begun experimentation of SA Terraform creation

--- _sub/compute/k8s-service-account/main.tf
+++ _sub/compute/k8s-service-account/main.tf
@@ -44,3 +44,51 @@ data "external" "get-token" {
   depends_on = [var.module_depends_on]
 
 }
+
+resource "kubernetes_service_account" "selfservice-deploy" {
+  metadata {
+    name = "selfservice-deploy"
+    namespace = "selfservice"
+
+  }
+
+  provider = kubernetes
+
+  depends_on = [var.module_depends_on]  
+}
+
+resource "kubernetes_cluster_role_binding" "selfservice-deploy" {
+  metadata {
+    name = "selfservice-deploy"
+    namespace = "selfservice"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "selfservice-deploy"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "selfservice-deploy"
+    namespace = "selfservice"
+  }
+
+  provider = kubernetes
+
+  depends_on = [var.module_depends_on]  
+}
+
+data "external" "get-token-selfservice-deploy" {
+  program = ["sh", "${path.module}/get-token.sh"]
+
+  query = {
+    cluster_name        = var.cluster_name
+    default_secret_name = kubernetes_service_account.selfservice-deploy.default_secret_name
+    kubeconfig_path     = var.kubeconfig_path # Research whether this is a directory path or a file path
+  }
+
+  depends_on = [var.module_depends_on]
+
+}
--- _sub/compute/k8s-service-account/outputs.tf
+++ _sub/compute/k8s-service-account/outputs.tf
@@ -2,3 +2,7 @@ output "deploy_user_config" {
   value = data.external.get-token.result["kubeconfig_json"]
 }
 
+output "selfservice_deploy_config" {
+  value = data.external.get-token-selfservice-deploy.result["kubeconfig_json"]
+}
+
